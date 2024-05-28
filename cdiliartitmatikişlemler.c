60246fceff7a9541585b63ed176b643a7c94f460
zorbeyyavas <yavaszorbey@gmail.com>
2024-05-29 01:55:47 +0300

    Add files via upload

---
+++ cdiliartitmatikişlemler.c
@@ -0,0 +1,49 @@
+int main(){
+    /*aritmetik operatörler 
+     toplama = "+" 
+    çıkartma = "-"
+    çarpma = "*"
+    bölme = "/"
+    mod alma = "%"
+    arttırma = "++"
+    azaltma = "--" */
+
+
+
+/*
+int x =6;
+int y=4;
+int z = x+y;
+printf("%d",z);
+*/
+
+/*int x =6;
+int y =4;
+int z =x-y;
+printf("%d",z);*/
+
+/*int x =6;
+int y =4;
+int z =x*y;
+printf("%d",z);*/
+
+
+/*int x =6;
+int y =4;
+float z =x/(float)y;
+printf("%.2f",z);*/
+//ya burda tam sonuç almak istiyorsa y float olucak yada float z =x/(float)y; yazıcaz
+
+
+/*int x =6;
+int y =4;
+int z =x%y;
+printf("%d",z);*/
+
+/*int x = 6;
+int y =4;
+x++;
+y++;
+x--;
+y--;*/
+}
